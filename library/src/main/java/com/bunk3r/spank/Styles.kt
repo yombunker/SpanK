d783ed58e99c4b28783522ab827d8a885053bf55
Jorge Aguilar <jorge@guilar.me>
2019-02-08 12:39:03 -0800

    - Fixing the Styles to look more like Kotlin code

--- library/src/main/java/com/bunk3r/spank/Styles.kt
+++ library/src/main/java/com/bunk3r/spank/Styles.kt
@@ -39,183 +39,127 @@ class SpankParagraph(
 /**
  * applies a BOLD style to the specified [SpankSection]
  */
-fun SpankSection.bold() {
-    applySpan(
-        StyleSpan(
-            Typeface.BOLD
-        )
-    )
-}
+fun SpankSection.bold() = applySpan(
+    StyleSpan(Typeface.BOLD)
+)
+
 
 /**
  * applies a ITALIC style to the specified [SpankSection]
  */
-fun SpankSection.italic() {
-    applySpan(
-        StyleSpan(
-            Typeface.ITALIC
-        )
-    )
-}
+fun SpankSection.italic() = applySpan(
+    StyleSpan(Typeface.ITALIC)
+)
 
 /**
  * applies an UNDERLINE style to the specified [SpankSection]
  */
-fun SpankSection.underline() {
-    applySpan(
-        UnderlineSpan()
-    )
-}
+fun SpankSection.underline() = applySpan(
+    UnderlineSpan()
+)
 
 /**
  * applies a STRIKE THROUGH style to the specified [SpankSection]
  */
-fun SpankSection.strikethrough() {
-    applySpan(
-        StrikethroughSpan()
-    )
-}
+fun SpankSection.strikethrough() = applySpan(
+    StrikethroughSpan()
+)
 
 /**
  * applies a SUBSCRIPT style to the specified [SpankSection]
  */
-fun SpankSection.subscript() {
-    applySpan(
-        SubscriptSpan()
-    )
-}
+fun SpankSection.subscript() = applySpan(
+    SubscriptSpan()
+)
 
 /**
  * applies a SUPERSCRIPT style to the specified [SpankSection]
  */
-fun SpankSection.superscript() {
-    applySpan(
-        SuperscriptSpan()
-    )
-}
+fun SpankSection.superscript() = applySpan(
+    SuperscriptSpan()
+)
 
 /**
  * applies a [color] FOREGROUND style to the specified [SpankSection]
  */
-fun SpankSection.foregroundColor(@ColorInt color: Int) {
-    applySpan(
-        ForegroundColorSpan(
-            color
-        )
-    )
-}
+fun SpankSection.foregroundColor(@ColorInt color: Int) = applySpan(
+    ForegroundColorSpan(color)
+)
 
 /**
  * applies a [color] BACKGROUND style to the specified [SpankSection]
  */
-fun SpankSection.backgroundColor(@ColorInt color: Int) {
-    applySpan(
-        BackgroundColorSpan(
-            color
-        )
-    )
-}
+fun SpankSection.backgroundColor(@ColorInt color: Int) = applySpan(
+    BackgroundColorSpan(color)
+)
 
 /**
  * applies a LINK style to the specified [SpankSection] which points to [url] and is clickable
  */
-fun SpankSection.link(url: String) {
-    applySpan(
-        URLSpan(
-            url
-        )
-    )
-}
+fun SpankSection.link(url: String) = applySpan(
+    URLSpan(url)
+)
 
 /**
  * applies the [fontFamily] FONT style to the specified [SpankSection]
  */
-fun SpankSection.font(@EzFontFamily fontFamily: String) {
-    applySpan(
-        TypefaceSpan(
-            fontFamily
-        )
-    )
-}
+fun SpankSection.font(@EzFontFamily fontFamily: String) = applySpan(
+    TypefaceSpan(fontFamily)
+)
 
 /**
  * applies all the styles in [appearanceRes] to the specified [SpankSection]
  */
-fun SpankSection.appearance(context: Context, appearanceRes: Int) {
-    applySpan(
-        TextAppearanceSpan(
-            context, appearanceRes
-        )
-    )
-}
+fun SpankSection.appearance(context: Context, appearanceRes: Int) = applySpan(
+    TextAppearanceSpan(context, appearanceRes)
+)
 
 /**
  * changes the LOCALE on the specified [SpankSection]
  */
 @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-fun SpankSection.locale(locale: Locale) {
-    applySpan(
-        LocaleSpan(
-            locale
-        )
-    )
-}
+fun SpankSection.locale(locale: Locale) = applySpan(
+    LocaleSpan(locale)
+)
 
 /**
  * applies a SCALE on the X axis of [proportion] to the specified [SpankSection]
  */
-fun SpankSection.scaleX(proportion: Float) {
-    applySpan(
-        ScaleXSpan(
-            proportion
-        )
-    )
-}
+fun SpankSection.scaleX(proportion: Float) = applySpan(
+    ScaleXSpan(proportion)
+)
 
 /**
  * applies a SCALING with [proportion] on both axis to the specified [SpankSection]
  */
-fun SpankSection.relativeSize(proportion: Float) {
-    applySpan(
-        RelativeSizeSpan(
-            proportion
-        )
-    )
-}
+fun SpankSection.relativeSize(proportion: Float) = applySpan(
+    RelativeSizeSpan(proportion)
+)
 
 /**
  * applies the SCALING necessary to change the size to [pixels] to the specified [SpankSection]
  */
-fun SpankSection.absoluteSize(pixels: Int) {
-    applySpan(
-        AbsoluteSizeSpan(
-            pixels
-        )
-    )
-}
+fun SpankSection.absoluteSize(pixels: Int) = applySpan(
+    AbsoluteSizeSpan(pixels)
+)
 
 /**
  * applies the SCALING necessary to change the size to [dips] to the specified [SpankSection]
  */
-fun SpankSection.absoluteSizeDP(dips: Int) {
-    applySpan(
-        AbsoluteSizeSpan(
-            dips, true
-        )
-    )
-}
+fun SpankSection.absoluteSizeDP(dips: Int) = applySpan(
+    AbsoluteSizeSpan(dips, true)
+)
 
 /**
  * applies a CLICK LISTENER to the specified [SpankSection], when clicked executes the [block] with
  * the part of the [SpankSection.content] from [SpankSection.start] to [SpankSection.end]
  */
-fun SpankSection.clickable(block: (CharSequence) -> Unit) {
-    applySpan(
-        ClickableSpank(
-            content.subSequence(start, end + 1), block
-        )
+fun SpankSection.clickable(block: (CharSequence) -> Unit) = applySpan(
+    ClickableSpank(
+        content.subSequence(start, end + 1),
+        block
     )
-}
+)
 
 /**
  * internal construct used as a intermediate between the Android framework and the library
@@ -224,21 +168,15 @@ private class ClickableSpank(
     val spanContent: CharSequence,
     val codeBlock: (CharSequence) -> Unit
 ) : ClickableSpan() {
-    override fun onClick(widget: View) {
-        codeBlock(spanContent)
-    }
+    override fun onClick(widget: View) = codeBlock(spanContent)
 }
 
 /**
  * adds an ANNOTATION to the specified [SpankSection] (No visual change, is just metadata)
  */
-fun SpankSection.customAnnotation(key: String, value: String) {
-    applySpan(
-        Annotation(
-            key, value
-        )
-    )
-}
+fun SpankSection.customAnnotation(key: String, value: String) = applySpan(
+    Annotation(key, value)
+)
 
 /**
  * replaces the specified [SpankSection] with the [drawable] aligned to [verticalAlignment],
@@ -248,15 +186,10 @@ fun SpankSection.image(
     drawable: Drawable,
     sourceUri: String? = null,
     verticalAlignment: Int = DynamicDrawableSpan.ALIGN_BOTTOM
-) {
-    val span =
-        if (sourceUri != null) ImageSpan(drawable, sourceUri, verticalAlignment)
-        else ImageSpan(drawable, verticalAlignment)
-
-    applySpan(
-        span
-    )
-}
+) = applySpan(
+    if (sourceUri != null) ImageSpan(drawable, sourceUri, verticalAlignment)
+    else ImageSpan(drawable, verticalAlignment)
+)
 
 /**
  * replaces the specified [SpankSection] with the image from [resId] aligned to [verticalAlignment],
@@ -266,13 +199,9 @@ fun SpankSection.imageResource(
     context: Context,
     @DrawableRes resId: Int,
     verticalAlignment: Int = DynamicDrawableSpan.ALIGN_BOTTOM
-) {
-    applySpan(
-        ImageSpan(
-            context, resId, verticalAlignment
-        )
-    )
-}
+) = applySpan(
+    ImageSpan(context, resId, verticalAlignment)
+)
 
 //*********************************************
 //*********************************************
@@ -289,47 +218,37 @@ fun SpankParagraph.customQuote(
     @ColorInt quoteColor: Int = -0xffff01,
     @IntRange(from = 0) stripeWidth: Int = 2,
     @IntRange(from = 0) gapWidth: Int = 2
-) {
-    applySpan(
-        QuoteSpan(quoteColor, stripeWidth, gapWidth)
-    )
-}
+) = applySpan(
+    QuoteSpan(quoteColor, stripeWidth, gapWidth)
+)
 
 /**
  * applies a QUOTE style to the specified [SpankParagraph] with [quoteColor]
  */
-fun SpankParagraph.quote(@ColorInt quoteColor: Int = -0xffff01) {
-    applySpan(
-        QuoteSpan(
-            quoteColor
-        )
-    )
-}
+fun SpankParagraph.quote(@ColorInt quoteColor: Int = -0xffff01) = applySpan(
+    QuoteSpan(quoteColor)
+)
 
 /**
  * aligns the specified [SpankParagraph] to [gravity]
  */
-fun SpankParagraph.alignTo(@EzAlign gravity: Int) {
-    applySpan(
-        AlignmentSpan.Standard(
-            when (gravity) {
-                EzAlign.START -> Layout.Alignment.ALIGN_NORMAL
-                EzAlign.CENTER -> Layout.Alignment.ALIGN_CENTER
-                EzAlign.END -> Layout.Alignment.ALIGN_OPPOSITE
-                else -> throw IllegalArgumentException("Unknown gravity $gravity, make sure to pass a valid value")
-            }
-        )
+fun SpankParagraph.alignTo(@EzAlign gravity: Int) = applySpan(
+    AlignmentSpan.Standard(
+        when (gravity) {
+            EzAlign.START -> Layout.Alignment.ALIGN_NORMAL
+            EzAlign.CENTER -> Layout.Alignment.ALIGN_CENTER
+            EzAlign.END -> Layout.Alignment.ALIGN_OPPOSITE
+            else -> throw IllegalArgumentException("Unknown gravity parameter: $gravity")
+        }
     )
-}
+)
 
 /**
  * aligns the specified [SpankParagraph] to [alignment]
  */
-fun SpankParagraph.customAlignTo(alignment: AlignmentSpan) {
-    applySpan(
-        alignment
-    )
-}
+fun SpankParagraph.customAlignTo(alignment: AlignmentSpan) = applySpan(
+    alignment
+)
 
 /**
  * applies a BULLET style to the specified [SpankParagraph] with [bulletColor] and [gapWidth]
@@ -337,57 +256,32 @@ fun SpankParagraph.customAlignTo(alignment: AlignmentSpan) {
 fun SpankParagraph.bullet(
     gapWidth: Int = BulletSpan.STANDARD_GAP_WIDTH,
     @ColorInt bulletColor: Int = Color.BLACK
-) {
-    applySpan(
-        BulletSpan(
-            gapWidth, bulletColor
-        )
-    )
-}
+) = applySpan(
+    BulletSpan(gapWidth, bulletColor)
+)
 
 /**
  * adds the [drawable] with some [padding] the specified [SpankParagraph]
  */
-fun SpankParagraph.drawableWithMargin(
-    drawable: Drawable?,
-    padding: Int = 0
-) {
-    if (drawable != null) {
-        applySpan(
-            DrawableMarginSpan(
-                drawable, padding
-            )
-        )
-    }
+fun SpankParagraph.drawableWithMargin(drawable: Drawable?, padding: Int = 0) = drawable?.let {
+    applySpan(
+        DrawableMarginSpan(it, padding)
+    )
 }
 
 /**
  * adds the [bitmap] with some [padding] the specified [SpankParagraph]
  */
-fun SpankParagraph.bitmapWithMargin(
-    bitmap: Bitmap,
-    padding: Int = 0
-) {
-    applySpan(
-        IconMarginSpan(
-            bitmap, padding
-        )
-    )
-}
+fun SpankParagraph.bitmapWithMargin(bitmap: Bitmap, padding: Int = 0) = applySpan(
+    IconMarginSpan(bitmap, padding)
+)
 
 /**
  * adds some [padding] to the first [numberOfLines] on the specified [SpankParagraph]
  */
-fun SpankParagraph.leadingMarginModifier(
-    padding: Int,
-    numberOfLines: Int
-) {
-    applySpan(
-        CustomLeadingMarginSpan(
-            padding, numberOfLines
-        )
-    )
-}
+fun SpankParagraph.leadingMarginModifier(padding: Int, numberOfLines: Int) = applySpan(
+    CustomLeadingMarginSpan(padding, numberOfLines)
+)
 
 /**
  * intermediate construct used by [leadingMarginModifier] to add [padding] on a [numberOfLines]
@@ -410,32 +304,22 @@ private class CustomLeadingMarginSpan(
         first: Boolean,
         layout: Layout?
     ) {
+        // STUB
     }
 
-    override fun getLeadingMargin(first: Boolean): Int {
-        return if (first) padding
-        else 0
-    }
+    override fun getLeadingMargin(first: Boolean) = if (first) padding else 0
 
-    override fun getLeadingMarginLineCount(): Int {
-        return numberOfLines
-    }
+    override fun getLeadingMarginLineCount() = numberOfLines
 }
 
 /**
  * applies a TAB STOP style with an [offset] to the specified [SpankParagraph]
  */
-fun SpankParagraph.tabStop(offset: Int) {
-    applySpan(
-        TabStopSpan.Standard(
-            offset
-        )
-    )
-}
+fun SpankParagraph.tabStop(offset: Int) = applySpan(
+    TabStopSpan.Standard(offset)
+)
 
 /**
  * internal method to apply the [span] to the content, used by all the styles
  */
-private fun SpankSection.applySpan(span: Any) {
-    content.setSpan(span, start, end + 1, spanFlag)
-}
+private fun SpankSection.applySpan(span: Any) = content.setSpan(span, start, end + 1, spanFlag)
